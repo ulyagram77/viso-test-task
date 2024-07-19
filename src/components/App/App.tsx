c6e6fce26197c9cc9ecf5bc1721654b584d89f61
kyrylo <kirillulianow@gmail.com>
2024-07-20 01:43:34 +0300

    added some setup of project

---
+++ src/components/App/App.tsx
@@ -0,0 +1,7 @@
+import './App.css';
+
+function App() {
+    return <div>Hellow Viso</div>;
+}
+
+export default App;
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+    plugins: [react()],
+    resolve: {
+        alias: {
+            src: '/src',
+            assets: '/src/assets',
+            components: '/src/components',
+            core: '/src/core',
+            styles: '/src/styles',
+        },
+    },
+});
