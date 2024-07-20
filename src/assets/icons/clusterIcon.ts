bf449582c9d28ecd438f2053b441101928191579
kyrylo <kirillulianow@gmail.com>
2024-07-20 16:00:19 +0300

    corrected types

--- src/assets/icons/clusterIcon.ts
+++ src/assets/icons/clusterIcon.ts
@@ -1,9 +1,9 @@
-import { divIcon, point, MarkerCluster, DivIcon } from 'leaflet';
+import L, { MarkerCluster } from 'leaflet';
 
-export const clusterIcon = function (cluster: MarkerCluster): DivIcon {
-    return new divIcon({
+export const clusterIcon = function (cluster: MarkerCluster) {
+    return L.divIcon({
         html: `<span class="cluster-icon">${cluster.getChildCount()}</span>`,
         className: 'custom-marker-cluster',
-        iconSize: point(33, 33, true),
+        iconSize: L.point(33, 33, true),
     });
 };
